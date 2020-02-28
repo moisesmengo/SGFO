4c92a9250e6ce291da4f291573be5496f0c33e38
moisesmengo <moisesmengo@live.com>
2020-02-27 21:26:17 -0300

    criando api de adminstradores

--- backend/api/admin.js
+++ backend/api/admin.js
@@ -1,7 +1,66 @@
+const bcrypt = require('bcrypt-nodejs')
+
 module.exports = app =>{
-    const save = (req, res) =>{
-        res.send('admin save')
+    const { existsOrError, notExistsOrError, equalsOrError } = app.api.validation
+    //função para criptografar a senha
+    const encryptPassword = password => {
+        const salt = bcrypt.genSaltSync(10)
+        return bcrypt.hashSync(password, salt)
+    }
+    const save = async (req, res) =>{
+        const admin = { ...req.body }
+        if(req.params.id) admin.id = req.params.id
+
+        //algumas validações
+        try{
+            existsOrError(admin.nome, 'Nome não informado')
+            existsOrError(admin.email, 'Email não informado')
+            existsOrError(admin.senha, 'Senha não informada')
+            existsOrError(admin.confirmarSenha, 'Confirmação de senha inválida')
+            equalsOrError(admin.senha, admin.confirmarSenha, 'Senhas não correspondem')
+
+            const adminFromDB = await app.db('admins')
+                .where({email: admin.email}).first()
+
+            if(!admin.id){
+                notExistsOrError(adminFromDB, 'O e-mail já está cadastrado')
+            }
+            
+        }catch(msg){
+            return res.status(400).send(msg)
+        }
+        //criptografar a senha e excluir a confirmação de senha
+        admin.senha = encryptPassword(admin.senha)
+        delete admin.confirmarSenha
+        //função para atualizar ou inserir
+        if(admin.id){
+            app.db('admins')
+                .update(admin)
+                .where({id: admin.id})
+                .then(_=> res.status(204).send())
+                .catch(err => res.status(500).send(err))
+        } else {
+            app.db('admins')
+                .insert(admin)
+                .then(_=> res.status(204).send())
+                .catch(err => res.status(500).send(err))
+        }
     }
 
-    return { save }
+    const get = (req,res)=>{
+        app.db('admins')
+            .select('id', 'nome', 'email')
+            .then(admins => res.json(admins))
+            .catch(err => res.status(500).send(err))
+    }
+
+    const getById = (req,res)=>{
+        app.db('admins')
+            .select('id', 'nome', 'email')
+            .where({id: req.params.id})
+            .then(admin => res.json(admin))
+            .catch(err => res.status(500).send(err))
+    }
+
+    return { save, get, getById }
 }
--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,4 +1,9 @@
 module.exports = app =>{
     app.route('/admins')
         .post(app.api.admin.save)
+        .get(app.api.admin.get)
+
+    app.route('/admins/:id')
+        .get(app.api.admin.getById)
+        .put(app.api.admin.save)
 }
