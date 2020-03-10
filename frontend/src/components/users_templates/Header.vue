ef10131f085351370ce76dc0cd86d34bfc1e0dba
moisesmengo <moisesmengo@live.com>
2020-03-09 21:03:12 -0300

    criando menu

--- frontend/src/components/users_templates/Header.vue
+++ frontend/src/components/users_templates/Header.vue
@@ -1,5 +1,21 @@
 <template>
-    <h1>Menu</h1>
+    <v-toolbar app>
+        <v-toolbar-title class="headline text-uppercase mr-4 " >
+            <span class="font-weight-light">Fornecimento de Óleo</span>
+        </v-toolbar-title>
+
+        <v-toolbar-items>
+            <v-btn flat to="/">Início</v-btn>
+            <v-btn flat to="/contato">Contato</v-btn>
+            <v-btn flat to="/sobre">Sobre</v-btn>
+        </v-toolbar-items>
+
+        <v-spacer></v-spacer>
+
+        <v-toolbar-items>
+            <v-btn flat to="/administracao">Área do Administrador</v-btn>
+        </v-toolbar-items>
+    </v-toolbar>
 </template>
 <script>
 export default {
