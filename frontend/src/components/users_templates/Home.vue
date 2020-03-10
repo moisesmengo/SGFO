0c42967f4fdfc6fe8beaac41f270efa3cf1ed079
moisesmengo <moisesmengo@live.com>
2020-03-09 22:47:47 -0300

    formulario de login adm

--- frontend/src/components/users_templates/Home.vue
+++ frontend/src/components/users_templates/Home.vue
@@ -1,5 +1,10 @@
 <template>
-    <h1>Início</h1>
+    <v-layout class="pink" row wrap>
+        <h1>inicio</h1>
+        <div class="col-6 col-md-4 yellow">
+                oi
+        </div>
+    </v-layout>
 </template>
 <script>
 export default {
--- frontend/src/components/users_templates/admin_login/LoginAdmin.vue
+++ frontend/src/components/users_templates/admin_login/LoginAdmin.vue
@@ -1,9 +1,46 @@
 <template>
-    <h1>Login ADM</h1>
+    <v-layout align-center justify-center>
+    <v-flex xs12 sm8 md4 >
+    <v-card class="elevation-5">
+        <v-toolbar class="blue-grey darken-2" dark flat >
+
+            <v-toolbar-title>Login Administração</v-toolbar-title>
+            <v-spacer></v-spacer>
+ 
+        </v-toolbar>
+        <v-card-text>
+        <v-form>
+            <v-text-field
+            label="Login"
+            name="login"
+            prepend-icon="person"
+            type="text"
+            ></v-text-field>
+
+            <v-text-field
+            id="senha"
+            label="Senha"
+            name="senha"
+            prepend-icon="lock"
+            type="password"
+            ></v-text-field>
+        </v-form>
+        </v-card-text>
+        <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn class="primary">Login</v-btn>
+        </v-card-actions>
+    </v-card>
+        </v-flex>
+    </v-layout>
 </template>
 <script>
 export default {
-    
+    data() {
+        return {
+            drawer: null,
+        }
+    },
 }
 </script>
 <style lang="css">
